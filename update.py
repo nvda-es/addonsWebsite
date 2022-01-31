e761be7ee715dadb540533df703198a51f392c19
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2022-01-31 10:20:16 +0100

    Updated add-on updater. Added Sound Splitter

--- update.py
+++ update.py
@@ -1,6 +1,7 @@
 import requests
 
 addresses=[
+"https://addons.nvda-project.org/addons/soundSplitter.es.html",
 "https://addons.nvda-project.org/addons/quickNotetaker.es.html",
 "https://addons.nvda-project.org/addons/ignoreBlanksIndentationReporting.es.html",
 "https://addons.nvda-project.org/addons/searchWith.es.html",
