d082a1f5e8eb43b7c551fcd032ac62056ec3c47c
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2022-06-28 09:12:42 +0200

    Added Becky. Updated WintenApps

--- update.py
+++ update.py
@@ -1,6 +1,7 @@
 import requests
 
 addresses=[
+"https://addons.nvda-project.org/addons/Becky.es.html",
 "https://addons.nvda-project.org/addons/cursorLocator.es.html",
 "https://addons.nvda-project.org/addons/speechLogger.es.html",
 "https://addons.nvda-project.org/addons/screenshots.es.html",
